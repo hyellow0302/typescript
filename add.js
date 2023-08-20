e0c72c28a4dc2ce084e7f033196b10f71d2e25a1
Hyellow0302 <lhi030210@gmail.com>
2023-08-20 16:50:31 +0900

    #1 TypeScript란?, TypeScript VS JavaScript 비교 분석

---
+++ add.js
@@ -0,0 +1,9 @@
+"use strict";
+function addTS(num1, num2) {
+    console.log(num1 + num2);
+}
+// add_2();
+// add_2(1);
+addTS(1, 2);
+// add_2(3, 4, 5);
+// add_2("hello", "world");
---
+++ add.ts
@@ -0,0 +1,9 @@
+function addTS(num1: number, num2: number) {
+  console.log(num1 + num2);
+}
+
+// add_2();
+// add_2(1);
+addTS(1, 2);
+// add_2(3, 4, 5);
+// add_2("hello", "world");
